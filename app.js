807364c4961eabfc78a1a22e10de6b050fd65d07
Jyotirmoy <jyotirmoydas12@gmail.com>
2023-05-07 09:30:21 +0530

    Added the course route

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const express = require('express')
 const homeRoutes = require('./routes/home')
+const courseRoutes = require('./routes/course')
 const app = express()
 
 // Use the following route when user hits the root '/'
 app.use('/', homeRoutes)
+app.use('/course', courseRoutes)
 
 app.listen(3000, () => {
     console.log('Server running on port 3000')
 })
-
-
