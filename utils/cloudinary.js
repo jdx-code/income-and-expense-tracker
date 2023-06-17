651503c2f9637e4218da8d341eeb02ce63523048
Jyotirmoy <jyotirmoydas12@gmail.com>
2023-06-17 21:01:59 +0530

    Applying styles to courses view page

--- utils/cloudinary.js
+++ utils/cloudinary.js
@@ -1,7 +1,7 @@
 require('dotenv').config({ path: './config/.env' });
 
 const cloudinary = require('cloudinary').v2;
-
+// Cloudinary config 
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
